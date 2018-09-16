fd4abfb4d7501877d0b26907223e5c5f923a63be
Hewerthon Adalberto <hewerthon@outlook.com>
2018-09-16 17:02:45 -0300

    modification

--- configuration.js
+++ configuration.js
@@ -1,7 +1,7 @@
 var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
 exports.config = {
 		framework: 'jasmine',
-		seleniumAddress: 'http://localhost:4444/wd/hub',
+		//seleniumAddress: 'http://localhost:4444/wd/hub',
 		//specs: ['Registration.js'],
 		specs: ['Jsobjectcall.js'],
 		onPrepare : function()
